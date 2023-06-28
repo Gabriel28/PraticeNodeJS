d54a01f90271c6bfaf96b209fc5bed236e49d8b6
Gabriel28 <nasc.gabriel.1997@gmail.com>
2023-06-28 00:11:28 -0300

    Change in script to modify start

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const nodemon = require('nodemon');
 
-nodemon({ script: 'index.js' })
+nodemon({ script: './server.js' })
   .on('start', console.clear)
   .on('restart', console.clear)
   .on('quit', () => {
