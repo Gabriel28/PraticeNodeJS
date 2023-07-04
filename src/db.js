ae8f012392d503448d9a819574293bbd27db3cae
Gabriel28 <nasc.gabriel.1997@gmail.com>
2023-07-04 01:29:29 -0300

    Add config to connection database

---
+++ src/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const dbConnection = mysql.createConnection({
+    host: process.env.MYSQL_DB_HOST,
+    port: process.env.MYSQL_DB_PORT,
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DB_NAME
+});
+
+dbConnection.connect((error) => {
+    if (error) {
+        console.error(`Error connecting to database: ${error.message}`);
+    } else {
+        console.log(`DB ${process.env.MYSQL_DB_NAME}: Connected successfully`);
+    }
+});
+module.exports = dbConnection;
